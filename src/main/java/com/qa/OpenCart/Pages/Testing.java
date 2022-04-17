d2069404d92f2d0c26ac204654d456c4cd4b0cd1
Sachin Patil <88768214+Sachin123-hash@users.noreply.github.com>
2022-04-17 10:29:23 +0530

    Create Testing.java

---
+++ src/main/java/com/qa/OpenCart/Pages/Testing.java
@@ -0,0 +1,29 @@
+package com.qa.OpenCart.Pages;
+
+import org.openqa.selenium.By;
+
+public class CartPage {
+	
+	private By cart=By.id("cart");
+	
+	
+
+	public static void main(String[] args) {
+		
+		
+
+		int i=10;
+		System.out.println(i);
+		
+		String f1="feature";
+		System.out.println(f1);
+		
+
+		String f2="feature";
+		System.out.println(f2);
+		
+		String f3="feature";
+		System.out.println(f2);
+	}
+
+}
